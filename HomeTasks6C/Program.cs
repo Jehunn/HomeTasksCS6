0e8b9f90303e3f47dbb94b6460d03adfb9ee69b2
Jehun <121166619+Jehunn@users.noreply.github.com>
2023-01-31 03:42:09 +0400

    Homework

--- HomeTasks6C/Program.cs
+++ HomeTasks6C/Program.cs
@@ -7,17 +7,7 @@ namespace HomeTasks6C
     {
         static void Main(string[] args)
         {
-            Product pr = new Product();
-            pr.Name = "Test";
-            Console.WriteLine(pr.Name);
-
-            Drink drink = new Drink();
-
-            pr.Price = 82;
-            drink.AlcoholPercent = 45;
-
-            Console.WriteLine(pr.Price);
-            Console.WriteLine(drink.AlcoholPercent);
+            Console.WriteLine("Test");
         }
     }
 }
